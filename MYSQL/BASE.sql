3f4b72ff64c27ba5b51c577ba1e73478ecbc958c
grDan92 <brdan992@gmail.com>
2021-09-08 08:43:44 -0300

    SQL's

---
+++ MYSQL/BASE.sql
@@ -0,0 +1,193 @@
+/* Modelagem Básica - 
+
+ENTIDADE=TABELA
+CAMPOS = ATRIBUTOS */
+
+CLIENTE
+
+NOME - CARACTER(30)
+CPF - NUMERICO(11)
+EMAIL - CARACTER(30)
+TELEFONE - CARACTER(30)
+ENDERECO - CARACTER(100)
+SEXO - CARACTER(1)
+
+/* PROCESSOS DE MODELAGEM  */
+
+/* FASE 01 E FASE 02 - AD ADM DE DADOS */
+
+MODELAGEM CONCEITUAL - RASCUNHO
+MODELAGEM LÓGICA - QUALQUER PROGRAMA DE MODELAGEM
+
+/* FASE 03 - DBA / AD */
+
+MODELAGEM FÍSICA - SCRIPTS DE BANCO
+
+/* INICIANDO A MODELAGEM FÍSICA */
+
+/* CRIANDO O BANCO DE DADOS */
+
+CREATE DATABASE EXEMPLO
+
+
+
+/*Tipos de dados */
+
+Todos os banos de dados possuem tipos que devem ser atribuidos aos dados de uma tabela.
+Para caracteres literais, temos char e varchar, para números temos int e float, para
+objetos como fotos e documentos, temos o blob, para textos extensos, temos o text. 
+A disciplina de banco de dados é tão fantástica que ao entendermos o porque das coisas,
+podemos iniciar já em modo avançado e um bom exemplo disso são os tipos. Há uma profissão
+dentro da área que é a do analista de performance ou tuning, esse profissional é responsável
+por analisar o banco de dados e deixá-lo mais rápido. Parece algo avançado, e é! Porém,
+com alguas atitudes simples, podemos deixar o banco sem a necessidade de atuação desse profissional.
+
+Cada caracter no banco de dados, vale 1 byte. Sendo assim, se eu entro com o dado JOÃO,
+estou entrando com 4 bytes no meu banco. E o que isso tem a ver com a tiagem de tabelas?
+
+O banco de dados funciona como um download de dados da internet. Se baixamos um arquivo de 1 giga,
+temos um temo maior que o download de 50 megas, considerando a mesma velocidade de conexão.
+
+Ao tiparmos uma tabela de modo errado ou displicente, vamos aumentar a quantidade de dados que 
+será baixada do banco de dados, prolongando assim o tempo de resposta.
+
+Uma comparacao bem didatica é o tipo char e varchar
+A palavra var, vem de variant, em ingles, ou seja, que é dinâmica. Logo, vimos que 1 caracter
+é igual a 1 byte. Vejamos então a tipagem
+
+varchar(10)
+char(10)
+
+entrando a palavra joao
+
+total de bytes varchar(10) = 4 bytes
+toal de bytes cahr(10) = 10 bytes
+
+/*isso ocorre pois o char não varia. Os caracteres restantes serao preenchidos com espaço. 
+eles nao ficam vazios. Enquanto que no varchar, o tipo varia conforme o dado.
+Entao utilizo sempre o varchar? Não. O charé ligeiramente mais performatico, por nao
+ter que gastar tempo variando de tamanho. Entao a regra é utilizar sempre o char quando
+sabemos que o numero de caracteres naquela coluna nao vai variar nunca. Por exemplo,
+unidade federativa, com dois digitos, sexo com um digito e assim por diante. Vista a diferença
+que podemos fazer com uma tipagem correta de tabelas, na próxima aula detalharemos os tipos do mysql
+e nos modulos específicos de cada banco, você entenderá os tipos correspondentes no sql server
+e no oracle, que mudam muito pouco.
+*/
+
+-- 2
+/* 1 MILHAO DE REGISTROS
+
+PARA CONTAR
+SELECT COUNT(*) FROM CLIENTE;
+
+SELECT SEXO, COUNT(*) FROM CLIENTE
+GROUP BY SEXO;
+
+SELECT CIDADE, COUNT(*) FROM CLIENTE
+GROUP BY CIDADE;
+
+CONDICAO
+SEXO = F
+CIDADE = RIO DE JANEIRO
+
+SITUACAO - TRATANDO COM OU / OR
+70% MULHERES = SEXO = F
+30% MORA NO RIO DE JANEIRO
+
+SELECT NOME, SEXO, ENDERECO
+FROM CLIENTE
+WHERE SEXO = 'F'
+OR CIDADE = 'RIO DE JANEIRO';
+
+SITUACAO - TRATANDO COM E / AND
+70% MULHERES = SEXO = F
+30% MORA NO RIO DE JANEIRO
+
+SELECT NOME, SEXO, ENDERECO
+FROM CLIENTE
+WHERE CIDADE = 'RIO DE JANEIRO'
+AND SEXO = 'F'; */
+
+
+/*  HISTORIA DA MODELAGEM 
+
+DECADA DE 70, EUA - CRISE DO SOFTWARE
+FOI A CRISE QUE ATINGIU NAO SOMENTE A AREA DE TECNOLOGIA
+MAS TAMBÉM TODAS AS EMPRESAS QUE UTILIZAVAM DE SISTEMAS
+TECNOLOGICOS PARA CONTROLAR SEUS NEGOCIOS.
+
+NAQUELA EPOCA, PROGRAMAR ERA UM PROCESSO ARTESANAL.
+BASTAVA OLHAR O CODIGO QUE VOCE SABERIA QUEM O
+ESCREVEU, TAO POUCOS ERAM OS PROGRAMADORES EACHTAO ARCAICO ERAM
+OS METODOS DE PROGRAMACAO. EM UM PRIMEIRO MOMENTO,
+OS PROFISSIONAIS DE TI VIRAM A NECESSIDADE DE SE PROGRAMAR
+E INICIARAM SUA CAMINHADA, CONSTRUINDO SISTEMAS QUE ATENDIAM
+NUM PRIMEIRO MOMENTO, MAS DEPOIS TRAZIAM INFORMACOES ERRADAS,
+PROVOCANDO A FALENCIA DE DIVERSAS EMPRESAS. LEMBRE-SE: INFORMACOES
+ERRADAS PODEM LEVAR A SUA EMPRESA A FALENCIA, E FOI ISSO QUE ACONTECEU.
+ISSO OCORRIA POIS OS PROFISSIONAIS SABIAM PROGRAMAR MAS ENTENDIAM APENAS
+DO NEGOCIO DAS EMPRESAS DAS QUAIS FAZIAM PARTE. SER TER O KNOW HOW
+DOS DEMAIS NEGOCIOS, OS PROGRAMADORES PROGRAMAVAM VOLTADOS PARA OS
+PROCEDIMENTOS, E ESSE FOI UM ERRO MORTAL.
+
+PETER CHEN, VENDO TODO ESSE CENARIO AFIRMOU: NAO PODEMOS CRIAR BANCOS
+DE DADOS VOLTADOS PARA PROCEDIMENTOS EMPRESARIAIS, POIS OS PROCEDIMENTOS
+SOFREM INTERFERENCIAS EXTERNAS E PODEM SER ALTERADOS. A UNICA COISA
+QUE NAO SOFRE ALTERACAO SAO OS DADOS.
+
+POR EXEMPLO, ARMAZENAMOS NO BANCO A VENDA DE JANEIRO, FEVEREIRO E MARCO
+O TOTAL DAS VENDAS E O IMPOSTO PAGO AO GOVERNO. 
+FAZEMOS ISSO DURANTE MUITO TEMPO. DE UMA HORA PRA OUTRA O GOVERNO
+MUDA A ALIQUOTA DO IMPOSTO. COMO FARÁ SENTIDO OS PAGAMENTOS ARMAZENADOS
+ANTERIORMENTE? AQUI NOTA-SE QUE ARMAZENAMOS DADOS E ARMAZENAMOS
+INFORMACOES CORRESPONDENTES AOS PROCEDIMENTOS EXTERNOS. DEVEMOS
+APENAS ARMAZENAR OS DADOS, POIS ELES SAO SUFICIENTES PARA GERAR
+A INFORMACAO.
+
+MODELAR É UMA ARTE, ALGO QUE SE APRENDE COM A PRATICA. E VOCES PODEM
+SE PERGUNTAR: MAS SE É ALGO QUE SE APRENDE COM A PRATICA, COMO QUEM
+ESTA COMEÇANDO, APRENDE?
+
+ASSIM COMO PETER CHEN CRIOU O MODELO DE ENTIDADE E RELACIONAMENTO, O MER,
+FORAM CRIADAS FORMAS NORMAIS, QUE SAO DIRETRIZES PARA APLICAR EM
+MODELAGEM DE AMBIENTES OLTP OU AMBIENTES TRANSACIONAIS DE BANCOS
+DE DADOS.
+
+HOJE TEMOS MAIS DO QUE 3 FORMAS NORMAIS, MAS AO NORMALIZARMOS UM
+BANCO ATE A SUA TERCEIRA FORMA, É O SUFICIENTE PARA TER UM
+TRABALHO LIMPO E LIVRE DE ERROS. VEREMOS AS TRES FORMAS NORMAIS,
+UMA A UMA, DETALHADAMENTE NAS PROXIMAS AULAS.
+*/
+
+
+
+
+/* PRIMEIRA FORMA NORMAL */
+
+/*
+1 - TODO CAMPO VETORIZADO SE TORNARÁ OUTRA TABELA
+
+[AMARELO, AZUL, LARANJA, VERDE] -> CORES
+
+[KA, FIESTA, UNO, CIVIC]-> CARROS
+
+2 - TODO CAMPO MULTIVALORADO SE TORNARÁ OUTRA TABELA.
+QUANDO O CAMPO FOR DIVISIVEL
+
+3 - TODA TABELA NECESSITA DE PELO MENOS UM CAMPO QUE IDENTIFIQUE TODO O REGISTRO COMO SENDO UNICO.
+ISSO É O QUE CHAMAMOS DE CHAVE PRIMARIA OU PRIMARY KEY.
+*/
+
+/* CADINALIDADE 
+
+- QUEM DEFINE A CARDINALIDADE É A REGRA DE NEGOCIOS */
+
+/* 
+PRIMEIRO ALGARISMO - OBRIGATORIEDADE 
+0 - NAO OBRIGATORIO
+1 - OBRIGATORIO
+
+SEGUNDO ALGARISMO - CARDINALIDADE
+1 - MAXIMO DE UM
+N - MAIS DE UM
+*/
